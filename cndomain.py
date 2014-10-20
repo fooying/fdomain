2f96cf28fe4a67964331262b7f709ec2c2a8c9fb
zhangzy <zhangzy@knownsec.com>
2014-10-20 18:07:34 +0800

    增加中文域名及根域名处理脚本

---
+++ cndomain.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Fooying@2014-10-20 17:11:14
+
+"""
+中文域名处理
+"""
+
+
+class CnDomain:
+
+    def _en_domain(self, domain):
+        domain_blocks = domain.split(".")
+        ret = []
+        for block in domain_blocks:
+            if self.if_cn_word(block):
+                block = "xn--" + block.decode("utf-8").encode("punycode")
+            ret.append(block)
+        return ".".join(ret)
+
+    def _cn_domain(self, domain):
+        domain = domain.lower()
+        domain_blocks = domain.split(".")
+        ret = []
+        for block in domain_blocks:
+            if block.find("xn--") == 0:
+                block = block[4:].decode("punycode").encode("utf-8")
+            ret.append(block)
+        return ".".join(ret)
+
+    def if_cn_word(self, word):
+        """
+        判断是否中文字符
+        """
+        ord0 = ord("0")
+        ord9 = ord("9")
+        orda = ord("a")
+        ordz = ord("z")
+        ord_ = ord("_")
+        is_cn = False
+        for w in word:
+            ordw = ord(w)
+            if not orda <= ordw <= ordz and not \
+                    ord0 <= ordw <= ord9 and ordw != ord_:
+                is_cn = True
+                break
+        return is_cn
+
+    def if_cn_domain(self, domain):
+        """
+        判断是否中文域名
+        """
+        domain_blocks = domain.split(".")
+        if_cn = False
+        for block in domain_blocks:
+            if self.if_cn_word(block):
+                if_cn = True
+                break
+        return if_cn
+
+    def format_domain(self, domain, to="en"):
+        if to == "cn":
+            domain = self._cn_domain(domain)
+        else:
+            domain = self._en_domain(domain)
+        return domain
---
+++ rootdomain.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding=utf-8
+# Fooying@2014-10-20 13:14:39
+
+"""
+根域名处理
+"""
+
+from suffixs import SUFFIXS
+
+
+class Domain:
+
+    def __unicode(self, value, code="gbk"):
+        try:
+            value = value.decode(code, "ignore")
+        except:
+            pass
+        return value
+
+    def __encode(self, value, code="utf-8"):
+        if isinstance(value, unicode):
+            try:
+                value = value.encode(code)
+            except UnicodeEncodeError:
+                value = value.encode("utf-8", "replace")
+        return value
+
+
+    def url_format(self, url):
+        """
+        return http(s)://www.example.com
+        """
+        if not url.startswith(("http://", "https://")):
+            url = "http://" + url
+        if url.endswith("/"):
+            url = url[:-1]
+        return url
+
+    def get_domain(self, url):
+        url = url.replace("https://", "")
+        url = url.replace("http://", "")
+        domain = url[:url.index("/")+1] if "/" in url else url
+
+    def get_root_domain(self, url):
+        domain = self.get_domain(url)
+        domain_blocks = domain.split(".")
+        index = -2
+        suffix = ".".join(domain_blocks[index:])
+        ifmatch = False
+        if suffix in SUFFIXS:
+            index -= 1
+            ifmatch = True
+        else:
+            index += 1
+            suffix = ".".join(domain_blocks[index:])
+            if suffix in SUFFIXS:
+                index -= 1
+                ifmatch = True
+        root_domain = ""
+        if ifmatch:
+            root_domain = ".".join(domain_blocks[index:])
+        return root_domain
