8fd63d65e206bd87e4af9b9d43868868f4a8674a
zhangzy <zhangzy@knownsec.com>
2014-10-21 11:07:18 +0800

    增加安装文件及相关调整

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+安装文件
+"""
+
+import os
+import sys
+
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+from fdomain import VERSION
+
+
+def get_reqs():
+    install_reqs = parse_requirements('requirements.txt')
+    return [str(ir.req) for ir in install_reqs]
+
+
+setup(
+    name='fdomain',
+    version=VERSION,
+    url='git@github.com:fooying/fdomain.git',
+    description='Domain Format Library by Fooying',
+    author='Fooying',
+    author_email='f00y1n9@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=get_reqs(),
+    test_suite='nose.collector',
+)
+
